864060e4864a2585c5a8aede5d9b3b586cbde061
SHOTAiiMURA <hn.j.shota027503@gamil.com>
2024-04-14 09:51:59 +0900

    modify function

--- Ui_Functions.py
+++ Ui_Functions.py
@@ -126,7 +126,7 @@ class Ui_Functions():
         #もし小数点が一回だけ有効なとき、リターン、デシモルをTrue
         self.decimal_on = True
         if self.decimal_on:
-            print(".")
+            self.ui.display_textLine.setText(".")
             self.decimal_on = False
 
         #add into text: self.ui.display_textLine.setText(**wanna display desicmal besids main number
